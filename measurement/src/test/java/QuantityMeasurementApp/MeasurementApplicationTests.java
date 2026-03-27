20b3b5d8583f2834c0fd9941acb9b989528331ca
apnacollage-work <keshavkapoor5911@gmail.com>
2026-03-27 14:17:11 +0530

    UC2

--- measurement/src/test/java/QuantityMeasurementApp/MeasurementApplicationTests.java
+++ measurement/src/test/java/QuantityMeasurementApp/MeasurementApplicationTests.java
@@ -38,6 +38,42 @@ class MeasurementApplicationTests {
 
         assertTrue(f1.equals(f1));
     }
+    @Test
+    void testInchEquality_SameValue(){
+        MeasurementApplication.Inch i1 = new MeasurementApplication.Inch(12.5);
+        MeasurementApplication.Inch i2 = new MeasurementApplication.Inch(12.5);
+
+        assertEquals(i1, i2);
+    }
+
+    @Test
+    void testInchEquality_DifferValue(){
+        MeasurementApplication.Inch i1 = new MeasurementApplication.Inch(12.5);
+        MeasurementApplication.Inch i2 = new MeasurementApplication.Inch(10.0);
+
+        assertNotEquals(i1, i2);
+    }
+
+    @Test
+    void testInchNullable_NullValue(){
+        MeasurementApplication.Inch i1 = new MeasurementApplication.Inch(20.0);
+
+        assertFalse(i1.equals(null));
+    }
+
+    @Test
+    void testInchEquality_ClassComparison(){
+        MeasurementApplication.Inch i1 = new MeasurementApplication.Inch(15.0);
+
+        assertFalse(i1.equals("Some String"));
+    }
+
+    @Test
+    void testInchEquality_SameReference(){
+        MeasurementApplication.Inch i1 = new MeasurementApplication.Inch(18.0);
+
+        assertTrue(i1.equals(i1));
+    }
 
 
 }
